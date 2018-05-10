7432961f99113680f955e3eb1944f297b06f4b50
guillermo <guillermo@guillermo>
2018-05-10 12:14:02 -0500

    Adding config file

--- bot.js
+++ bot.js
@@ -1,22 +1,18 @@
-var Bot     = require('telegram-api').default
+var Bot     = require("telegram-api").default
 var Message = require('telegram-api/types/Message')
 
+var config = require("./config")
+
 var bot = new Bot({
-	token: '577001210:AAF1q8ZxC9kpSyFbbF7fKgfThJwAxkEi41Y'
+	token: config.telegramApiKey
 })
 
 bot.start()
 
-bot.get(/Hola/, (message) => {
-	var answer = new Message().text('HI DUDE').to(message.chat.id)
+bot.command("start", (msg) => {
+	var answer = new Message()
+		.text("Ejecutando el comando start")
+		.to(msg.chat.id)
 
 	bot.send(answer)
-})
-
-bot.command('start', (message) => {
-	var welcome = new Message().text('Welcome to arquitectura bot').to(message.chat.id)
-
-	bot.send(welcome)
-}).catch((error) => {
-	console.log(error)
 })
---
+++ config.js
@@ -0,0 +1,5 @@
+var config = {
+	telegramApiKey: "577001210:AAF1q8ZxC9kpSyFbbF7fKgfThJwAxkEi41Y"
+}
+
+module.exports = config
