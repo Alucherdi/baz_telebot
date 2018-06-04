f74749448e325dd748098a9e14a36fe8f547b8b6
Alucherdi <hjgo127942@gmail.com>
2018-06-04 18:44:49 -0500

    Removing express server

--- bot.js
+++ bot.js
@@ -1,9 +1,3 @@
-var express   = require("express")
-var bp        = require("body-parser")
-var app = express()
-
-app.use(bp.json())
-
 const TeleBot = require("telebot")
 const config  = require("./config")
 const utils   = require("./utils")
@@ -26,7 +20,7 @@ bot.on(/\w/, (msg) => {
 			dest: number[0],
 			idTelegram: msg.message_id
 		}).then(data => {
-			msg.reply.text(`Se ha detectado el número: ${number}`)
+			msg.reply.text(`Se ha detectado el número: ${number}, se procede a mandar el mensaje`)
 		})
 	} else {
 		msg.reply.text("No se encontró el número o es un número no valido")
@@ -35,12 +29,3 @@ bot.on(/\w/, (msg) => {
 
 console.log("Starting bot")
 bot.start()
-
-app.post("/telegramCallback", (req, res) => {
-	var body = req.body
-	bot.sendMessage(body.idTelegram, JSON.stringify(body))
-	res.send({
-		code: 200
-	})
-})
-app.listen(8099, () => console.log("Listening port 8099"))
