398b6824a447812307f19114483f50da8af9a4a2
guillermo <guillermo@10.51.145.105>
2018-07-23 17:20:48 -0500

    Changing topic

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 var config = {
 	telegramApiKey: "577001210:AAF1q8ZxC9kpSyFbbF7fKgfThJwAxkEi41Y",
 	googleApiKey:   "key=AAAAsISpCi0:APA91bG6Orm8wZmCDCgRMKLlkZSQgF5RjVJc0gLx6wDXCekzuXgM1QXurRinNIn0sotQhxMkpJByX5A5E201d87fYpr91vAh5qY8p_nWf9kCeh583JJPBxiGtjUDWcxOtjtCyW6_gmRu",
-	topic:          "dC0PB8UcjfI:APA91bFfAbG8dwBn1nP-x6roc3ztkiWSJ7WyO55gEdLBwlQjS_aGallMT2CbA-pNmH_OkcakSlExxxoj4UYVTqQfzMo5p58l-dhbS6Oh30sad-ESsKIPZy4TQzRgmGvsXbxfkXRD-ZDS"
+	topic:          "/topics/rataclient"
 }
 
 module.exports = config
